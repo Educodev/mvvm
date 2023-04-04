89e0525f730188c018eba590f4174032b6c358a5
Eduardo Herrera <edauardodesarrollador@icloud.com>
2023-04-04 18:41:30 +0200

    Initial Commit

---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Eduardo Herrera on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
